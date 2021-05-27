818a5c599623be273196f1f794197c744850b924
Trevor Allen <trevor2492@gmail.com>
2021-05-27 16:49:29 -0500

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Button, Alert, TextInput } from 'react-native';
 
 export default function App() {
+
+  const [name, onChangeText] = useState('')
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={name}
+      />
+
+      <Text>Hello {name}</Text>
+
+      <Button 
+        title='Tap Here'
+        onPress={() => Alert.alert(name + ' pressed the button')}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +33,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    height: 40,
+    width: 150,
+    margin: 12
   },
 });
